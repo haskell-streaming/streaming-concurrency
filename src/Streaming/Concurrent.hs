3b7154356658341eaa873b496c50854820e59864
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-07-03 16:18:36 +1000

    Working version!
    This seems to handle the issue in pipes-concurrency:
    https://github.com/Gabriel439/Haskell-Pipes-Concurrency-Library/issues/27
    
    There is still some undefined/unsafe behaviour present when used with
    buffers of size <= 0, but unless I wanted to put Maybes around buffer
    creation there's not much more I can do.

--- src/Streaming/Concurrent.hs
+++ src/Streaming/Concurrent.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 {- |
    Module      : Streaming.Concurrent
    Description : Concurrency support for the streaming ecosystem
@@ -5,52 +7,84 @@
    License     : MIT
    Maintainer  : Ivan.Miljenovic@gmail.com
 
+   Consider a physical desk for someone that has to deal with
+   correspondence.
 
+   A typical system is to have two baskets\/trays: one for incoming
+   papers that still needs to be processed, and another for outgoing
+   papers that have already been processed.
+
+   We use this metaphor for dealing with 'Buffer's: data is fed into
+   one using the 'InBasket' (until the buffer indicates that it has
+   had enough) and taken out from the 'OutBasket'.
 
  -}
-module Streaming.Concurrent where
+module Streaming.Concurrent
+  ( -- * Buffers
+    Buffer
+  , unbounded
+  , bounded
+  , latest
+  , newest
+    -- * Using a buffer
+  , withBuffer
+  , InBasket(..)
+  , OutBasket(..)
+    -- * Stream support
+  , writeStreamBasket
+  , readStreamBasket
+  , mergeStreams
+  ) where
 
 import           Streaming
-import qualified Streaming.Prelude as P
+import qualified Streaming.Prelude as S
 
+import Control.Concurrent.Async.Lifted (concurrently, forConcurrently_)
+import Control.Monad.Trans.Control (MonadBaseControl)
+import Control.Monad.Base (liftBase, MonadBase)
 import Control.Concurrent.STM
-import Control.Monad.Catch    (MonadMask, bracket, finally)
+import Control.Monad.Catch             (MonadMask, bracket, bracket_)
+import Control.Monad (when)
 
 --------------------------------------------------------------------------------
 
--- | Consider a physical desk for someone that has to deal with
---   correspondence.
+-- | Concurrently merge multiple streams together.
 --
---   A typical system is to have two baskets\/trays: one for incoming
---   papers that still needs to be processed, and another for outgoing
---   papers that have already been processed.
---
---   We use this metaphor for dealing with 'Buffer's: data is fed into
---   one using the 'inBasket' (until the buffer indicates that it has
---   had enough) and taken out from the 'outBasket'.
-data Baskets a = Baskets
-  { inBasket  :: !(a -> STM Bool)
-    -- ^ Feed input into a buffer.  Returns 'False' if no more input
-    --   is accepted.
-  , outBasket :: !(STM (Maybe a))
-    -- ^ Remove a value from a buffer.  Returns 'Nothing' if no more
-    --   values are available.
-  }
+--   The resulting order is unspecified.
+mergeStreams :: (MonadMask m, MonadBaseControl IO m)
+                => Buffer a -> [Stream (Of a) m v]
+                -> (Stream (Of a) m () -> m r) -> m r
+mergeStreams buff strs f = withBuffer buff
+                                      (forConcurrently_ strs . flip writeStreamBasket)
+                                      (f . readStreamBasket)
 
-withBuffer :: (MonadMask m, MonadIO m)
-              => Buffer a
-              -> (Baskets a -> m r)
-              -> m r
-withBuffer buf f = bracket (liftIO (spawnBaskets buf))
-                           (liftIO . atomically . snd)
-                           (f . fst)
+-- | Write a single stream to a buffer.
+--
+--   Type written to make it easier if this is the only stream being
+--   written to the buffer.
+writeStreamBasket :: (MonadBase IO m) => Stream (Of a) m r -> InBasket a -> m ()
+writeStreamBasket stream (InBasket send) = go stream
+  where
+    go str = do eNxt <- S.next str -- uncons requires r ~ ()
+                case eNxt of
+                  Left  _         -> return ()
+                  Right (a, str') -> do
+                    continue <- liftBase (atomically (send a))
+                    when continue (go str')
+
+-- | Read the output of a buffer into a stream.
+readStreamBasket :: (MonadBase IO m) => OutBasket a -> Stream (Of a) m ()
+readStreamBasket (OutBasket receive) = S.untilRight getNext
+  where
+    getNext = maybe (Right ()) Left <$> liftBase (atomically receive)
 
 --------------------------------------------------------------------------------
 -- This entire section is almost completely taken from
 -- pipes-concurrent by Gabriel Gonzalez:
 -- https://github.com/Gabriel439/Haskell-Pipes-Concurrency-Library
 
--- | 'Buffer' specifies how to buffer messages stored within the mailbox
+-- | 'Buffer' specifies how to buffer messages between our 'InBasket'
+--   and our 'OutBasket'.
 data Buffer a
     = Unbounded
     | Bounded Int
@@ -59,31 +93,80 @@ data Buffer a
     | Newest Int
     | New
 
--- | Store an unbounded number of messages in a FIFO queue
+-- | Store an unbounded number of messages in a FIFO queue.
 unbounded :: Buffer a
 unbounded = Unbounded
 
--- | Store a bounded number of messages, specified by the 'Int' argument
+-- | Store a bounded number of messages, specified by the 'Int'
+--   argument.
+--
+--   A buffer size @<= 0@ will result in a permanently empty buffer,
+--   which could result in a system that hang.
 bounded :: Int -> Buffer a
 bounded 1 = Single
 bounded n = Bounded n
 
-{-| Only store the 'Latest' message, beginning with an initial value
-    'Latest' is never empty nor full.
--}
+-- | Only store the \"latest\" message, beginning with an initial
+--   value.
+--
+--   This buffer is never empty nor full; as such, it is up to the
+--   caller to ensure they only take as many values as they need
+--   (e.g. using @'S.print' . 'readStreamBasket'@ as the final
+--   parameter to 'withBuffer' will -- after all other values are
+--   processed -- keep printing the last value over and over again).
 latest :: a -> Buffer a
 latest = Latest
 
-{-| Like @Bounded@, but 'send' never fails (the buffer is never full).
-    Instead, old elements are discard to make room for new elements
--}
+-- | Like 'bounded', but 'sendMsg' never fails (the buffer is never
+--   full).  Instead, old elements are discard to make room for new
+--   elements.
+--
+--   As with 'bounded', providing a size @<= 0@ will result in no
+--   values being provided to the buffer, thus no values being read
+--   and hence the system will most likely hang.
 newest :: Int -> Buffer a
 newest 1 = New
 newest n = Newest n
 
-spawnBaskets :: Buffer a -> IO (Baskets a, STM ())
-spawnBaskets buffer = do
-    (writeB, readB) <- case buffer of
+-- | An exhaustible source of values.
+--
+--   'receiveMsg' returns 'Nothing' if the source is exhausted.
+newtype OutBasket a = OutBasket { receiveMsg :: STM (Maybe a) }
+
+-- | An exhaustible sink of values.
+--
+--   'sendMsg' returns 'False' if the sink is exhausted.
+newtype InBasket a = InBasket { sendMsg :: a -> STM Bool }
+
+-- | Use a buffer to asynchronously communicate.
+--
+--   Two functions are taken as parameters:
+--
+--   * How to provide input to the buffer (the result of this is
+--     discarded)
+--
+--   * How to take values from the buffer
+--
+--   As soon as one function indicates that it is complete then the
+--   other is terminated.  This is safe: trying to write data to a
+--   closed buffer will not achieve anything.
+--
+--   However, reading a buffer that has not indicated that it is
+--   closed (e.g. waiting on an action to complete to be able to
+--   provide the next value) but contains no values will block.
+withBuffer :: (MonadMask m, MonadBaseControl IO m)
+              => Buffer a -> (InBasket a -> m i)
+              -> (OutBasket a -> m r) -> m r
+withBuffer buffer sendIn readOut =
+  bracket
+    (liftBase openBasket)
+    (\(_, _, _, seal) -> liftBase (atomically seal)) $
+      \(writeB, readB, sealed, seal) ->
+        snd <$> concurrently (withIn writeB sealed seal)
+                             (withOut readB sealed seal)
+  where
+    openBasket = do
+      (writeB, readB) <- case buffer of
         Bounded n -> do
             q <- newTBQueueIO n
             return (writeTBQueue q, readTBQueue q)
@@ -104,30 +187,31 @@ spawnBaskets buffer = do
             let writeB x = writeTBQueue q x <|> (tryReadTBQueue q *> writeB x)
             return (writeB, readTBQueue q)
 
-    sealed <- newTVarIO False
-    let seal = writeTVar sealed True
+      -- We use this TVar as the communication mechanism between
+      -- inputs and outputs as to whether either sub-continuation has
+      -- finished.
+      sealed <- newTVarIO False
+      let seal = writeTVar sealed True
 
-    {- Use weak TVars to keep track of whether the 'Input' or 'Output' has been
-       garbage collected.  Seal the mailbox when either of them becomes garbage
-       collected.
-    -}
-    rSend <- newTVarIO ()
-    void $ mkWeakTVar rSend (atomically seal)
-    rRecv <- newTVarIO ()
-    void $ mkWeakTVar rRecv (atomically seal)
+      return (writeB, readB, sealed, seal)
 
-    let sendOrEnd a = do
-            b <- readTVar sealed
-            if b
-                then return False
-                else do
-                    writeB a
-                    return True
+    withIn writeB sealed seal =
+      bracket_ (return ())
+               (liftBase (atomically seal))
+               (sendIn (InBasket sendOrEnd))
+      where
+        sendOrEnd a = do
+          canWrite <- not <$> readTVar sealed
+          when canWrite (writeB a)
+          return canWrite
+
+    withOut readB sealed seal =
+      bracket_ (return ())
+               (liftBase (atomically seal))
+               (readOut (OutBasket readOrEnd))
+      where
         readOrEnd = (Just <$> readB) <|> (do
-            b <- readTVar sealed
-            check b
-            return Nothing )
-        inp a = sendOrEnd a <* readTVar rSend
-        out   = readOrEnd   <* readTVar rRecv
-    return (Baskets inp out, seal)
-{-# INLINABLE spawnBaskets #-}
+          b <- readTVar sealed
+          check b
+          return Nothing )
+{-# INLINABLE withBuffer #-}
